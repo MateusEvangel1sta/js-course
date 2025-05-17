794fdc36b29852ecb8608d4f338cb8c929ce5b47
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2025-05-17 17:08:14 -0300

    Basic - Initialization

---
+++ js-course/basic/01_console.js
@@ -0,0 +1,5 @@
+console.log("Luiz 'Otávio'"); // String
+console.log('Luiz "Otávio"');
+console.log(`'Luiz' "Otávio"`);
+
+console.log(123, 15.85); // Number
---
+++ js-course/basic/02_comments.js
@@ -0,0 +1,10 @@
+// This is a single line comment
+
+/**
+  *This is a
+  * Comment
+  * multiple
+  */
+
+// Comments are ignored by the engine responsible for running the JavaScript.
+// // They are used to describe something in your code.
