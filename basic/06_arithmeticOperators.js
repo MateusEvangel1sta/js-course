30aac6b657b598bc30941c9f94eabde8d5b31969
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2025-05-19 15:28:17 -0300

    Basic - Save

--- basic/06_arithmeticOperators.js
+++ basic/06_arithmeticOperators.js
@@ -3,19 +3,30 @@ const otherNumber = 2;
 
 // Addition
 console.log(number + otherNumber);
-
 // Subtraction
 console.log(number - otherNumber);
-
 // Multiplication
 console.log(number * otherNumber);
-
 // Division
 console.log(number / otherNumber);
-
 // Power
 console.log(number ** otherNumber);
-
 // Rest of the division
 console.log(number % otherNumber); // return 0
 
+let counter = 1;
+counter++; // increment
+console.log(counter);
+
+counter--; // decrement
+console.log(counter);
+
+// Assignment operations
+counter += 2;
+console.log(counter);
+
+counter -= 2;
+console.log(counter);
+
+counter *= 10;
+console.log(counter);
