0268cb566bf8d4e4355bf2270c29d6fe9e53f848
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2025-05-18 23:23:38 -0300

    Basic - Save

---
+++ basic/05_primitiveData.js
@@ -0,0 +1,17 @@
+const name = `Luiz`; // string
+const number = 10; // number - int
+const otherNumber = 10.5; // number - float
+
+let studentName; // undefined -> It does not point to the place in memory.
+let studentSurname = null; // null -> It does not point to the place in memory.
+
+const boolean = true; // true or false.
+
+console.log(
+  typeof name,
+  typeof number,
+  typeof otherNumber,
+  typeof studentName,
+  typeof studentSurname,
+  typeof boolean
+);
---
+++ basic/06_arithmeticOperators.js
@@ -0,0 +1,21 @@
+const number = 10;
+const otherNumber = 2;
+
+// Addition
+console.log(number + otherNumber);
+
+// Subtraction
+console.log(number - otherNumber);
+
+// Multiplication
+console.log(number * otherNumber);
+
+// Division
+console.log(number / otherNumber);
+
+// Power
+console.log(number ** otherNumber);
+
+// Rest of the division
+console.log(number % otherNumber); // return 0
+
