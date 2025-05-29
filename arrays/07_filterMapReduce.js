16c362f7d34432af9650238115f657d7176e8918
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2025-05-29 15:00:50 -0300

    Array - Completed

---
+++ arrays/07_filterMapReduce.js
@@ -0,0 +1,8 @@
+const numbers = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+const filtrados = numbers
+  .filter((el) => el % 2 === 0)
+  .map((el) => el * 2)
+  .reduce((acc, el) => (acc += el));
+
+console.log(filtrados);
